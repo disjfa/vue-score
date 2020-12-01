6fe88319dbed13294f58b047282e5b2e0592f35c
disjfa <disjfa@disjfa.nl>
2020-12-01 21:16:32 +0100

    Updates

---
+++ precache-manifest.1ade744558009dc9671bee4fc858c31f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf344fd0018945001d59",
+    "url": "/vue-score/css/app.bc52b548.css"
+  },
+  {
+    "revision": "8b51ba77579a3f8de644",
+    "url": "/vue-score/css/basic-counter.2871c629.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-score/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "f536be4885f23122f11dc4f878046a5c",
+    "url": "/vue-score/index.html"
+  },
+  {
+    "revision": "7bb6ae50d5da7bc9779f",
+    "url": "/vue-score/js/about.66cc2584.js"
+  },
+  {
+    "revision": "bf344fd0018945001d59",
+    "url": "/vue-score/js/app.3bb0ab86.js"
+  },
+  {
+    "revision": "8b51ba77579a3f8de644",
+    "url": "/vue-score/js/basic-counter.d7f6d675.js"
+  },
+  {
+    "revision": "6671b4ef795598d04c9b",
+    "url": "/vue-score/js/chunk-vendors.60d0757c.js"
+  },
+  {
+    "revision": "840a50a7fa30cab5955c06349f85f1ec",
+    "url": "/vue-score/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-score/robots.txt"
+  }
+]);
